fab02a4c03856e370025acae8cd6ce16ea93b1ab
Angelcarmichael <91912584+Angelcarmichael@users.noreply.github.com>
2022-11-14 15:15:37 -0600

    Gave Up

---
+++ Assets/Scripts/MunuHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MunuHandler : MonoBehaviour
+{
+    [SerializeField] Text PlayerNameInput;
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
